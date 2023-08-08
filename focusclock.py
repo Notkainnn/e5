b6a4cd342b8e34b2426c7dc05a86c3996debdc86
Notkainnn <141737576+Notkainnn@users.noreply.github.com>
2023-08-08 20:37:04 +0800

    Create focusclock.py

---
+++ focusclock.py
@@ -0,0 +1,8 @@
+import time
+minutes = int(input(*Enter the number of minutes to focus: D)
+seconds = minutes 60}
+while seconds:
+mins, secs = divmod(seconds, 60)
+timer = '{:020};(:2d}* •format(mins, print (timer, end="\r") time.sleep(1) seconds -= 1
+＄eCS）
+print ("Time"s up!")
